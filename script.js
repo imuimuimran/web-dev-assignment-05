7e791dbbb251f62d494977496ad9ae8e6c5e9c93
Mohd Imran Hossain <hossainmohdimran@gmail.com>
2025-08-28 03:22:21 +0600

    project is primarily done

--- script.js
+++ script.js
@@ -13,9 +13,11 @@ for (let i = 0; i < hearts.length; i++) {
 
 // Call with Coin
 const callByCoin = document.getElementById('call-by-coin');
-let availableCoin = parseInt(callByCoin.innerText, 10);
+let availableCoin = parseInt(callByCoin.querySelector('span').innerText, 10);
 
 const btnCall = document.getElementsByClassName('btn-call');
+const callHistoryContainer = document.getElementById('call-history-container');
+const btnClear = document.getElementById('btn-clear');
 
 for(let j = 0; j < btnCall.length; j++){
     btnCall[j].addEventListener('click', function(){
@@ -29,11 +31,54 @@ for(let j = 0; j < btnCall.length; j++){
             if(availableCoin >= 20){
                 availableCoin -= 20;
                 callByCoin.querySelector('span').innerText = availableCoin;
+
+                const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+
+                const historyDiv = document.createElement('div');
+                historyDiv.classList.add('call-history', 'rounded-xl', 'bg-[#fafafa]', 'mt-2', 'flex', 'justify-between', 'items-center', 'p-4');
+
+                historyDiv.innerHTML = `
+                    <div>
+                        <h2 class="text-lg md:text-xl font-semibold">${serviceName}</h2>
+                        <h2 class="text-gray-500 text-lg">${serviceNumber}</h2>
+                    </div>
+                    <h2 class="text-sm md:text-xl font-semibold">${currentTime}</h2>
+                `;
+
+                callHistoryContainer.appendChild(historyDiv);
             } 
             else{
                 alert("You don't have enough coin to call.");
             }
         }
+
+        
     });
+    
 };
 
+btnClear.addEventListener('click', function () {
+    callHistoryContainer.innerHTML = "";
+});
+
+
+// Count and Copy
+const btnCopyNavbar = document.getElementById('btn-copy-navbar');
+let copyCount = 0;
+
+const btnCopy = document.getElementsByClassName('btn-copy');
+
+for (let k = 0; k < btnCopy.length; k++) {
+    btnCopy[k].addEventListener('click', function () {
+        const cardItem = btnCopy[k].closest('.card-item');
+        const serviceNumber = cardItem.querySelector('.service-number').innerText.trim();
+
+        navigator.clipboard.writeText(serviceNumber).then(() => {
+            alert("The Number is copied");
+            copyCount++;
+            btnCopyNavbar.querySelector("span").textContent = copyCount;
+        }).catch(err => {
+            console.error("Failed to copy number: ", err);
+        });
+    });
+}
